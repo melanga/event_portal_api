a305dd2952c43e01e3299b0fea1e56f3291154d3
Melanga Dissanayake <dissanayakedmmb@gmail.com>
2022-07-09 19:04:30 +0530

    database created and connected to webstorm

--- db.sql
+++ db.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE eventportal;
 
-CREATE TABLE user (
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
@@ -16,16 +18,23 @@ CREATE TABLE user (
 CREATE TABLE customer (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id uuid NOT NULL,
-    CONSTRAINT customer_user_id_fk FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+    CONSTRAINT customer_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE service_provider(
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     description VARCHAR(255),
     user_id uuid NOT NULL,
-    CONSTRAINT service_provider_user_id_fk FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+    CONSTRAINT service_provider_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+CREATE TABLE category (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(50) NOT NULL,
+    icon_url VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
 
 CREATE TABLE event (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -36,7 +45,7 @@ CREATE TABLE event (
     category_id uuid NOT NULL,
     CONSTRAINT event_category_id_fk FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE service_provider_event (
@@ -44,15 +53,7 @@ CREATE TABLE service_provider_event (
     event_id uuid NOT NULL,
     PRIMARY KEY (service_provider_id, event_id),
     CONSTRAINT service_provider_event_service_provider_id_fk FOREIGN KEY (service_provider_id) REFERENCES service_provider(id) ON DELETE SET NULL,
-    CONSTRAINT service_provider_event_event_id_fk FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE,
-);
-
-CREATE TABLE category (
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR(50) NOT NULL,
-    icon_url VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    CONSTRAINT service_provider_event_event_id_fk FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE
 );
 
 CREATE TABLE requirement (
@@ -63,5 +64,5 @@ CREATE TABLE requirement (
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     CONSTRAINT requirement_event_id_fk FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE,
-    CONSTRAINT requirement_category_id_fk FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE SET NULL ,
+    CONSTRAINT requirement_category_id_fk FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE SET NULL
 );
