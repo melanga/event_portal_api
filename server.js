d9d5b12fe77ab4579dca59dd055ebb52ef5d5b33
Melanga Dissanayake <dissanayakedmmb@gmail.com>
2022-07-10 18:15:01 +0530

    override default error handler

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+require('express-async-errors');
 require('dotenv').config();
-const app = express();
 
+const { errorHandler } = require('./middleware/errorMiddleware');
+// initialize express app
+const app = express();
+// define the port
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+// initialize routes
+app.use('/api/v1/users', require('./routes/usersRoutes'));
+// override default error handler
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`server is up and listening on port ${port}`);
